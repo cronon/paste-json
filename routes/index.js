0c3bc757b32b8cbedbcecb7a596743c2da1c5f9a
cronon <cron@example.com>
2015-04-27 16:29:12 +0300

    done with backend

--- routes/index.js
+++ routes/index.js
@@ -11,27 +11,49 @@ exports.index = function(req, res){
 
 // get /:id.html
 exports.binHtml = function(req, res){
-  res.render('index', {
-    title: 'res',
-    bin: req.params.id
-  });
+  var key = parseInt(req.params.id, 10)
+  db.collection('bins').findOne({key: key}, function(err, result){
+    if (err) throw err;
+    var bin;
+    if (result === null) {
+      bin = '';
+    } else {
+      bin = result.bin;
+    }
+
+    res.render('index', {
+      title: 'res',
+      bin: bin
+    });
+  })
+  
 };
 
 // get /:id
 exports.bin = function(req, res){
-  res.send(req.params.id)
+  var key = parseInt(req.params.id, 10)
+  db.collection('bins').findOne({key: key}, function(err, result){
+    if (err) throw err;
+    var bin;
+    if (result === null) {
+      bin = '';
+    } else {
+      bin = result.bin;
+    }
+    res.type('json');
+    res.send(bin);
+  })
 };
 
 // post /
 exports.createBin = function(req, res){
-    db.collection('lastIndex').findOne(function(err, result){
-      var oldKey = result.key;
-      var newKey = oldKey + 1;
-      db.collection('lastIndex').update({key: oldKey}, {key: newKey}, function(err){
-        res.render('index', {
-          title: 'res',
-          bin: newKey
-        });   
-      })
-    })   
+  db.collection('lastIndex').findOne(function(err, result){
+    var oldKey = result.key;
+    var newKey = oldKey + 1;
+    db.collection('lastIndex').update({key: oldKey}, {key: newKey}, function(err){
+      db.collection('bins').insert({key: newKey, bin: req.body.bin}, function(err){
+        res.redirect('/'+newKey+'.html')
+      });
+    })
+  })   
 }
